fceeaeda7cc96c9b5c065a0bfaf2c631cd414977
chloe3c <chloe3c@209-1>
2024-11-11 14:40:10 +0200

    Добавьте файлы проекта.

---
+++ LABA3.4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double startX = -5;
+        double endX = 10;
+        double step = 0.2;
+
+        for (double x = startX; x <= endX; x += step)
+        {
+            double y = CalculateFunction(x);
+            Console.WriteLine($"x: {x:F1}, y: {y:F4}");
+        }
+    }
+
+    static double CalculateFunction(double x)
+    {
+        if (x > 12.5)
+        {
+            return Math.Pow(x, 2) + 2.5;
+        }
+        else if (x >= 0 && x <= 12.5)
+        {
+            return Math.Exp(x - Math.Sin(x)) + Math.Cos(0.001 * x);
+        }
+        else // x < 0
+        {
+            return 1 - Math.Pow(x, 2);
+        }
+    }
+}
+
